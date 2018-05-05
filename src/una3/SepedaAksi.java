8b974583b78358eb9fea2c947ab14ffd18fee033
unamahlddin <https://github.com/unamahlddn>
2018-05-05 18:52:43 +0800

    Instance, buat atrribut dan panggil method

--- src/una3/SepedaAksi.java
+++ src/una3/SepedaAksi.java
@@ -1,6 +1,11 @@
 package una3;
 public class SepedaAksi {
     public static void main(String[] args) {
+        Sepeda s = new Sepeda();
+        s.merk="Kupu-Kupu";
+        s.warna="Merah";
+        s.harga=1000;
         
+        s.cetakInfo();
     }
 }
