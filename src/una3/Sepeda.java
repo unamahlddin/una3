7882cd6e4dc686af50a700536991418578729eda
unamahlddin <https://github.com/unamahlddn>
2018-05-05 18:48:51 +0800

    buat method cetak()

--- src/una3/Sepeda.java
+++ src/una3/Sepeda.java
@@ -4,4 +4,10 @@ public class Sepeda {
     String merk;
     String warna;
     double harga;
+    
+    void cetakInfo(){
+        System.out.println("Merk \t: "+merk+"\n"+
+                "Warna \t: "+warna+"\n"+
+                "Harga \t:"+harga);
+    }
 }
