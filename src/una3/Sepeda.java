52b5f84e2260d86e82a4f2a693723719f87851b0
unamahlddin <https://github.com/unamahlddn>
2018-05-05 18:55:06 +0800

    buat getter and setter

--- src/una3/Sepeda.java
+++ src/una3/Sepeda.java
@@ -1,13 +1,40 @@
 package una3;
 
 public class Sepeda {
-    String merk;
-    String warna;
-    double harga;
+    private String merk;
+    private String warna;
+    private double harga;
     
     void cetakInfo(){
         System.out.println("Merk \t: "+merk+"\n"+
                 "Warna \t: "+warna+"\n"+
                 "Harga \t:"+harga);
     }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+    
+    
+    
 }
