d8fc28f8a84d462b850c29021f6957a54e3876f4
unamahlddin <https://github.com/unamahlddn>
2018-05-05 18:59:44 +0800

    menggunakan method get

--- src/una3/SepedaAksi.java
+++ src/una3/SepedaAksi.java
@@ -9,5 +9,11 @@ public class SepedaAksi {
         
         s.cetakInfo();
         
+        System.out.print("Merknya \t: ");
+        System.out.println(s.getMerk());
+        System.out.print("Warnanya \t: ");
+        System.out.println(s.getWarna());
+        System.out.print("Harganya \t: ");
+        System.out.println(s.getHarga());
     }
 }
